68e83c6e76aa50aef0764faa6c0a8dee917b7f36
Caylin James <cjames@leapagency.com>
2023-05-26 15:35:55 -0400

    add save and move line keymaps

--- mappings.lua
+++ mappings.lua
@@ -32,7 +32,11 @@ return {
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
+  },
+  v = {
+    ["<S-j>"] = ":m '>+1<cr>gv=gv",
+    ["<S-k>"] = ":m '<-2<cr>gv=gv",
   },
   t = {
     -- setting a mapping to false will disable it
