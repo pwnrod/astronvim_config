b22dc2120681b4d16bfb23a0d757b670fdb8c4f6
Caylin James <cjames@leapagency.com>
2023-08-15 15:28:34 -0400

    bring back catppuccin

---
+++ plugins/catppuccin.lua
@@ -0,0 +1,18 @@
+return {
+  "catppuccin",
+  opts = {
+    transparent_background = true,
+    integrations = {
+      telescope = { enabled = true, style = "nvchad" },
+      harpoon = true,
+      mason = true,
+      gitsigns = true,
+      indent_blankline = true,
+      mini = true,
+      neotree = true,
+      noice = true,
+      notify = true,
+      which_key = true,
+    },
+  },
+}
--- plugins/community.lua
+++ plugins/community.lua
@@ -5,7 +5,7 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.motion.mini-surround" },
   -- not quite ready to embrace this one yet
